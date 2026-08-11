5b8b49ccdbded3c947df3fef423a71cb42338b6e
Aurelia Davine Putri Nata <aurel.nata@zetta-intern.space>
2026-08-11 15:49:24 +0700

    update academic_school

--- academic_school/src/app/features/dashboard/pages/dashboard.spec.ts
+++ academic_school/src/app/features/dashboard/pages/dashboard.spec.ts
@@ -1,26 +1,13 @@
 //********** ANGULAR TESTING IMPORT **********
-//Import Angular utilities for component testing.
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-
-//********** COMPONENT IMPORT **********
-// Import the component to be tested.
 import { Dashboard } from './dashboard';
 
-
-//********** COMPONENT TEST SUITE **********
-// Groups all tests for Dashboard.
+//********** TEST SUITE **********
 describe('Dashboard', () => {
   let component: Dashboard;
   let fixture: ComponentFixture<Dashboard>;
 
-
   //********** TEST ENVIRONMENT SETUP **********
-  /**
-   * Configure the testing module and create the component.
-   *  Create the component instance.
-   *  Wait until the component is stable.
-   */
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Dashboard],
@@ -31,10 +18,6 @@ describe('Dashboard', () => {
 
     await fixture.whenStable();
   });
-
-
-  //********** COMPONENT CREATION TEST **********
-  // Verify that the component is created successfully.
   it('should create', () => {
     expect(component).toBeTruthy();
   });
--- academic_school/src/app/features/dashboard/pages/dashboard.ts
+++ academic_school/src/app/features/dashboard/pages/dashboard.ts
@@ -1,17 +1,10 @@
 
-//********** ANGULAR COMPONENT IMPORT **********
-// Import Component decorator from Angular.
+//********** Angular Imports **********
 import { Component } from '@angular/core';
-
-//********** COMPONENT CONFIGURATION **********
-// Defines the component selector, template, and styling.
 @Component({
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-
-//********** ASSESSMENTS CREATE COMPONENT **********
-// Handles the Assessment Dashboard page.
 export class Dashboard {}
