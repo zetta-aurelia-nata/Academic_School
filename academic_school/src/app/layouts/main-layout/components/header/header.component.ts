def323ea8f284af07f1c557c3e116476836e8c3a
Aurelia Davine Putri Nata <aurel.nata@zetta-intern.space>
2026-08-13 01:05:18 +0700

    commit responsiv page

--- academic_school/src/app/layouts/main-layout/components/header/header.component.ts
+++ academic_school/src/app/layouts/main-layout/components/header/header.component.ts
@@ -1,9 +1,14 @@
 //********** ANGULAR IMPORTS **********
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
+  imports: [MatIcon],
 })
-
-export class HeaderComponent {}
+export class HeaderComponent {
+  toggleSidebar() {
+    throw new Error('Method not implemented.');
+  }
+}
--- academic_school/src/app/layouts/main-layout/main-layout.component.ts
+++ academic_school/src/app/layouts/main-layout/main-layout.component.ts
@@ -1,22 +1,30 @@
 //********** Angular imports **********
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Output, EventEmitter } from '@angular/core';
 
 //********** Application Modules & Component Imports **********
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
-
 @Component({
   selector: 'app-main-layout',
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    SidebarComponent,
-    FooterComponent,
-  ],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, FooterComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
+export class MainLayout {
+  //********** PUBLIC STATE VARIABLES **********
+  isSidebarOpen = true;
 
-export class MainLayout {}
+  //********** ACTION HANDLERS **********
+  toggleSidebar(): void {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  closeSidebar(): void {
+    this.isSidebarOpen = false;
+  }
+  
+  @Output() menuToggle = new EventEmitter<void>();
+}
